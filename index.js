34999d9585ebd89e53c696bc128cf17f567a236a
Kazi Rahat <kazirahat1007@outlook.com>
2023-01-09 10:43:18 +0600

    create body parser using express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 /* Middleware */
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("e-shop is server is running");
